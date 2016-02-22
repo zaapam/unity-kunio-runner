33b386d74e2b671ae384867bdd74c9b09211ae17
Zaapam <settapak@gmail.com>
2016-02-22 21:49:27 +0700

    Add jump action to character
    - Add jump action to character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private bool _jumping = false;
 	public Animator anim;
+	public Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		rigidbody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float v = Input.GetAxis ("Horizontal") * 1.5f;
 		anim.SetFloat ("speed", Mathf.Abs(v));	
-		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (v, 0.0f);
+		rigidbody.velocity = new Vector2 (v, rigidbody.velocity.y);
 		if (Input.GetAxis ("Horizontal") > 0) {
 			//transform.Translate (Vector2.right * 1.5f * Time.deltaTime);
 			transform.eulerAngles = new Vector2 (0, 0);
@@ -24,6 +27,21 @@ public class PlayerController : MonoBehaviour {
 		}
 
 
+
+		if (Input.GetButton ("Jump") && !_jumping) {
+			_jumping = true;
+			anim.SetBool ("jump", true);
+			rigidbody.AddForce (Vector2.up * 200.0f);
+		}
 		//Debug.Log ("Speed: " + v);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "Ground") {
+			_jumping = false;
+			anim.SetBool ("jump", false);
+		}
+
+		Debug.Log ("Collll-" + _jumping);
 	}
 }
